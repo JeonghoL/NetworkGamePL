bf6ec00a102bcbe2d57364b0f8002b60169af543
Vallhup <ths1220ths@naver.com>
2025-11-26 05:05:01 +0900

    회전 완성, 공격 구현 중

--- PEW & PEW Server/PEW & PEW Server/Character.cpp
+++ PEW & PEW Server/PEW & PEW Server/Character.cpp
@@ -1,5 +1,5 @@
 #include "Character.h"
-#include "protocol.hpp"
+#include "protocol.h"
 #include "GameObject.h"
 
 #include <algorithm>
@@ -17,6 +17,107 @@ void Character::Update(const float dT)
 inline bool Character::IsAlive() const
 {
 	return false;
+}
+
+void Character::TakeDamage(int damage)
+{
+	if (not _isAlive) {
+		return;
+	}
+
+	_hp -= damage;
+	if (_hp <= 0) {
+		Death();
+	}
+}
+
+std::optional<vec3> Character::GetNextProjectile(float nowTime)
+{
+	if (not _attackSeq.has_value()) {
+		return std::nullopt;
+	}
+
+	auto& seq = _attackSeq.value();
+	if (seq.attackTimes.empty()) {
+		_attackSeq.reset();
+		return std::nullopt;
+	}
+
+	if (nowTime >= seq.attackTimes.front()) {
+		seq.attackTimes.erase(seq.attackTimes.begin());
+
+		if (seq.attackTimes.empty()) {
+			_attackSeq.reset();
+		}
+
+		return seq.direction;
+	}
+
+	return std::nullopt;
+}
+
+std::pair<vec3, vec3> Character::GetCollisionBox() const
+{
+	const vec3 offsetMin{ SIZE_X / 2, 0, SIZE_Z / 2 };
+	const vec3 offsetMax{ SIZE_X / 2, SIZE_Y, SIZE_Z / 2 };
+
+	return { position - offsetMin, position + offsetMax };
+}
+
+void Character::SetInput(float angle, char direction, bool isRun)
+{
+	if (_angle != angle) {
+		_angle = angle;
+		_angleChange = true;
+	}
+
+	_direction = direction;
+	_isRun = isRun;
+}
+
+void Character::SetAttackSequence(float nowTime, const vec3& dir)
+{
+	float attackOffset =
+		_isRun ? 0.43f :
+		IsMove() ? 0.56f :
+		0.45f;
+
+	if (_attackSeq.has_value()) {
+		return;
+	}
+
+	std::vector<float> attackTimes(NUMBER_OF_ATTACK);
+	std::generate_n(attackTimes.begin(), NUMBER_OF_ATTACK,
+		[n = 0, &nowTime, &attackOffset, this]() mutable
+		{
+			return nowTime + attackOffset + (n++) * INTERVAL_OF_ATTACK;
+		});
+
+	_attackSeq = AttackSequence{ dir, attackTimes };
+}
+
+void Character::Death()
+{
+	_hp = 0;
+	_isAlive = false;
+
+	_direction = -1;
+	_isRun = false;
+	_angleChange = false;
+}
+
+void Character::Revive()
+{
+	position = DEFAULT_POS[id % 2];
+	_angle = 0.0f;
+	_angleChange = false;
+
+	_hp = MAX_HP;
+	_isAlive = true;
+	_isDeadProcessed = false;
+
+	_direction = -1;
+	_isRun = false;
 }
 
 void Character::Move(const float dT)
@@ -52,3 +153,4 @@ void Character::Move(const float dT)
 
 	version++;
 }
+
--- PEW & PEW Server/PEW & PEW Server/Character.h
+++ PEW & PEW Server/PEW & PEW Server/Character.h
@@ -1,14 +1,43 @@
 #pragma once
 
+#include <optional>
+#include <vector>
 #include "GameObject.h"
 
 class Character : public GameObject {
+	struct AttackSequence {
+		vec3 direction;
+		std::vector<float> attackTimes;
+	};
+
+	static constexpr int NUMBER_OF_ATTACK{ 3 };
+	static constexpr float INTERVAL_OF_ATTACK{ 0.15f };
+
+	static constexpr float SIZE_X{ 0.5f };
+	static constexpr float SIZE_Y{ 0.95f };
+	static constexpr float SIZE_Z{ 0.4f };
+
+	static constexpr int MAX_HP{ 100 };
+
+	static constexpr vec3 DEFAULT_POS[2] = {
+		{ 13.6f, 0.0f, 0.0f },
+		{ -13.6f, 0.0f, 0.0f }
+	};
+
 public:
 	Character(int i);
 	virtual ~Character() = default;
 
 	virtual void Update(const float dT) override;
 	virtual bool IsAlive() const override;
+
+	void TakeDamage(int damage);
+	std::optional<vec3> GetNextProjectile(float nowTime);
+	std::pair<vec3, vec3> GetCollisionBox() const;
+	void SetInput(float angle, char direction, bool isRun);
+	void SetAttackSequence(float nowTime, const vec3& dir);
+	void Death();
+	void Revive();
 
 	float GetAngle() const { return _angle; }
 	bool IsMove() const { return _direction >= 0 and _direction < 8; }
@@ -21,5 +50,14 @@ private:
 	bool _isRun;
 
 	float _angle;
+	bool _angleChange;
+
+	int _hp;
+	bool _isAlive;
+	bool _isDeadProcessed;
+
+	int _texture;
+
+	std::optional<AttackSequence> _attackSeq;
 };
 
