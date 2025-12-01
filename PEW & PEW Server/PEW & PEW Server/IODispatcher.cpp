b536d2af84f7aca3b38cae63370b0231fc8028f0
Vallhup <ths1220ths@naver.com>
2025-12-01 15:50:16 +0900

    충돌 및 후처리 (표창 삭제, 캐릭터 사망) 구현

--- PEW & PEW Server/PEW & PEW Server/IODispatcher.cpp
+++
@@ -1,9 +0,0 @@
-#include "IODispatcher.h"
-
-void IODispatcher::Broadcast(const std::vector<char>& packet)
-{
-	for (auto& sendQueue : sendQueues)
-	{
-		sendQueue.push(packet);
-	}
-}
--- PEW & PEW Server/PEW & PEW Server/Main.cpp
+++ PEW & PEW Server/PEW & PEW Server/Main.cpp
@@ -1,6 +1,25 @@
-#include "ServerCore.h"
+#include "pch.h"
 
 int main()
 {
-	ServerCore::Get().Start();
+	setlocale(LC_ALL, "korean");
+
+	Logger::Init();
+	Logger::SetLevel(LogLevel::Info);
+
+	auto service = std::make_shared<Service>();
+	
+	if (not service->Init()) {
+		LOG_ERR("Service Init Failed");
+		return -1;
+	}
+
+	service->Start();
+
+	//std::cout << "종료하려면 Enter 키를 누르세요...\n";
+	//std::cin.get();
+
+	service->Stop();
+
+	return 0;
 }
